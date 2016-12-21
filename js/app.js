483b6c2b94a9950e4a72f0b08e09999881d04eba
arthurk6 <developersysteminformation@gmail.com>
2016-12-21 09:22:05 -0300

    Change Height Main Banner

--- js/app.js
+++ js/app.js
@@ -3,10 +3,10 @@ $('#about').parallax({
 	speed: 0.8,
 })
 .css({
-	backgroundImage: 'linear-gradient(rgba(0,0,0,.5) 60%, transparent)',
-	height: $(window).height();
+	backgroundImage: 'linear-gradient(rgba(0,0,0,.5) 60%, transparent)'
 });
 
+$('.header-img').css('height', $(window).height());
 $('#rodape').parallax({
 	imageSrc: '../img/bg-footer.jpg',
 	speed: 0.7
