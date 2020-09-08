7af263e43adeb2e2907ce60321e006c92d3b18a5
developersysteminformation <developersysteminformation@gmail.com>
2020-09-08 15:03:55 -0400

    Upload files webmotors challenge

--- webmotors/service-worker.js
+++ webmotors/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2d054ff072d61909b8cf702d2037dcb3.js"
+  "/webmotors/precache-manifest.d3d21ce093732cf26169273aa2c728a6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/webmotors/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
