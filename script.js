9e3bc5f766a9f9fa21a2d327b117f0176303cc24
arthurlmenezes <developersysteminformation@gmail.com>
2025-02-14 23:32:36 -0300

    updated code to missing content on start page

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     // Pergunta ao usuário
     let resposta = "";
     do {
+        document.querySelector(".content").style.display = "none";
         resposta = prompt("Quem é o amor da sua vida? Digite o primeiro nome");
 
         if (resposta === null || resposta.trim() === "") {
