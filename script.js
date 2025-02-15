631a45f5f6045786841f85d43333170c00a2d43d
arthurlmenezes <developersysteminformation@gmail.com>
2025-02-14 23:23:39 -0300

    Update script to empty answer

--- script.js
+++ script.js
@@ -4,5 +4,10 @@ document.addEventListener('DOMContentLoaded', function() {
     do {
         resposta = prompt("Quem é o amor da sua vida? Digite o primeiro nome");
 
+        if (resposta === null || resposta.trim() === "") {
+            alert("Você precisa responder. Tente novamente!");
+            resposta = "";  // Reinicia para repetir a pergunta
+        }
+
     } while (resposta.toLocaleLowerCase() != 'Arthur'.toLowerCase());
 });
